be1a5fa8a00356bdc92bc78c747050e23d6efdb6
Myra Gull <myragull749@gmail.com>
2025-07-12 06:30:51 +0500

    Made a function connectToDb for the connection and export it

--- cohort-classes/121-Database/src/db/bd.js
+++ cohort-classes/121-Database/src/db/bd.js
@@ -1,1 +1,10 @@
-const mongoose= require("mongose"); 
+const mongoose = require("mongose");
+
+function connectToDB() {
+  mongoose.connect("connection-string").then(() => {
+    console.log("Connected to database");
+  });
+}
+ 
+
+module.exports =connectToDB; 
