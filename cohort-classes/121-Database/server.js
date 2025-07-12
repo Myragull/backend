448dd2f1173044fe39633f1a8a4c504c091063c0
Myra Gull <myragull749@gmail.com>
2025-07-12 06:10:27 +0500

    Install express created the server object run the server make two apis and tested in post man

--- cohort-classes/121-Database/server.js
+++ cohort-classes/121-Database/server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log("Server is running");
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/notes", (req, res) => {
+  const { title, content } = req.body;
+  console.log(title, content);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running");
+});
